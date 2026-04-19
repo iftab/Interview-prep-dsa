b0b683df95cc6c7f6684fed3cd0570150a9853c2
Munsi Md Iftabuddin <39688213+iftab@users.noreply.github.com>
2026-04-19 23:27:42 +0530

    Update ArrayQueue.java
    Circular Queue Implementation.

--- ArrayQueue/src/com/learn/queue/ArrayQueue.java
+++ ArrayQueue/src/com/learn/queue/ArrayQueue.java
@@ -14,13 +14,24 @@ public class ArrayQueue {
     }
 
     public void enqueue(Employee employee) {
-        if(back == queue.length){
+        if(size() == queue.length - 1){
+            int numItems = size();
             System.out.println("Re-sizing the queue");
             Employee[] newQueue = new Employee[queue.length * 2];
-            System.arraycopy(queue, 0, newQueue, 0,queue.length);
-            this.queue = newQueue;
+            System.arraycopy(queue, front,newQueue, 0, queue.length - front);
+            System.arraycopy(queue, 0, newQueue, queue.length - front, back);
+
+            queue = newQueue;
+            front = 0;
+            back = numItems;
         }
-        queue[back++] = employee;
+
+        queue[back] = employee;
+        if(back < queue.length){
+            back++;
+        }else{
+            back = 0;
+        }
         size++;
     }
 
@@ -34,6 +45,8 @@ public class ArrayQueue {
        if(back - front == 0){
            front = 0;
            back = 0;
+       }else if(front == queue.length){
+           front = 0;
        }
        size--;
 
@@ -48,13 +61,27 @@ public class ArrayQueue {
     }
 
     public void printQueue(){
-        for(int i = front; i < back; i++){
-            System.out.println(queue[i]);
+        if(front <=back){
+            for(int i = front; i < back; i++){
+                System.out.println(queue[i]);
+            }
+        }else {
+            for(int i = front; i < queue.length; i++){
+                System.out.println(queue[i]);
+            }
+            for(int i = 0; i < back; i++){
+                System.out.println(queue[i]);
+            }
         }
-    }
+        }
 
-    public int size(){
-        return back - front;
+
+    public int size() {
+        if (front <= back) {
+            return back - front;
+        } else {
+            return queue.length - front + back;
+        }
     }
 
 }
